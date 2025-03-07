1da06956b23aaeb01a04bb84e1e3c704879b9603
Zachary-Fulkman <163482286+Zachary-Fulkman@users.noreply.github.com>
2025-03-07 18:21:30 -0500

    Add project files.

---
+++ AutomatedDataBackup/BackupScheduler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedDataBackup
+{
+    class BackupScheduler
+    {
+        private System.Timers.Timer _timer;
+        private BackupService _backupService;
+
+        public BackupScheduler()
+        {
+            _backupService = new BackupService();
+            _timer = new System.Timers.Timer(3600000);
+            _timer.Elapsed += OnElapsedTime;
+            _timer.AutoReset = true;
+        }
+
+        // Calls method when timer ends
+        private void OnElapsedTime(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Running backup...");
+
+            // Calls the Backup Data method from the BackupService class
+            _backupService.BackupData(@"C:BusinessData", @"D:\Backup");
+        }
+        public void Start() => _timer.Start();
+        public void Stop() => _timer.Stop();
+    }
+}
---
+++ AutomatedDataBackup/BackupService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedDataBackup
+{
+    class BackupService
+    {
+        //Backs up data from a source directory to a destination directory
+        public void BackupData(string sourceDirectory, string backupDirectory)
+        {
+            try
+            {
+                // Tries to check if a source directory exists
+                if (Directory.Exists(sourceDirectory))
+                {
+                    // Creates a backup directory if not
+                    if (!Directory.Exists(backupDirectory))
+                    {
+                        Directory.CreateDirectory(backupDirectory); // Creates the directory
+                    }
+
+                    // Gets files from the source directory
+                    var files = Directory.GetFiles(sourceDirectory);
+
+                    foreach (var file in files)
+                    {
+                        string destinationPath = Path.Combine(backupDirectory, Path.GetFileName(file));
+
+                        //Copies file to the backup
+                        File.Copy(file, destinationPath, overwrite: true);
+
+                        Console.WriteLine($"Backed up {file} to {destinationPath}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Source directory does not exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error during backup: {ex.Message}");
+            }
+        }
+    }
+}
