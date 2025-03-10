c635df12d4115c13c7a87558e845eeea898c815b
Zachary-Fulkman <163482286+Zachary-Fulkman@users.noreply.github.com>
2025-03-09 20:25:34 -0400

    Update Form1.cs - Advanced Options
    -added Time of Day scheduler

--- AutomatedDataBackup/Form1.cs
+++ AutomatedDataBackup/Form1.cs
@@ -7,6 +7,8 @@ namespace AutomatedDataBackupUI
 {
     public partial class Form1 : Form
     {
+        private System.Windows.Forms.Timer _backupTimer;
+        private DateTime _scheduledBackupTime;
         private BackupScheduler _backupScheduler;
         private BackupLog _logger;
 
@@ -15,20 +17,55 @@ namespace AutomatedDataBackupUI
             InitializeComponent();
             _logger = new BackupLog();
             _backupScheduler = new BackupScheduler(_logger);
+            _backupScheduler.Start();
+
+            dateTimePickerTime = new DateTimePicker();
+            dateTimePickerTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            dateTimePickerTime.ShowUpDown = false;
+            dateTimePickerTime.Location = new System.Drawing.Point(20, 60);
+            dateTimePickerTime.Size = new System.Drawing.Size(150, 20);
+            this.Controls.Add(dateTimePickerTime);
+
+            _backupTimer = new System.Windows.Forms.Timer();
+            _backupTimer.Interval = 60000; // Check every minute
+            _backupTimer.Tick += BackupTimer_Tick;
         }
 
-        private void btnStop_Click(object sender, EventArgs e)
+        private void BackupTimer_Tick(object sender, EventArgs e)
         {
-            lblStatus.Text = "Stopping backup...";
-            _backupScheduler.Stop();
-            lblStatus.Text = "Backup stopped.";
+            if (DateTime.Now >= _scheduledBackupTime)
+            {
+                StartBackup();
+                _backupTimer.Stop();
+            }
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            _scheduledBackupTime = dateTimePickerTime.Value;
+            _backupTimer.Start();
+            lblStatus.Text = "Backup scheduled for " + _scheduledBackupTime.ToString("HH:mm");
+        }
+
+        private void StartBackup()
+        {
             lblStatus.Text = "Starting backup...";
-            _backupScheduler.Start();
-            lblStatus.Text = "Backup started.";
+            BackupService backupService = new BackupService(_logger);
+            backupService.BackupData(@"C:\BusinessData", @"D:\Backup");
+            lblStatus.Text = "Backup completed.";
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = "Stopping periodic backup...";
+            _backupScheduler.Stop();  // Stops the periodic backup
+            lblStatus.Text = "Periodic backup stopped.";
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            _backupTimer.Stop();
+            lblStatus.Text = "Scheduled backup cancelled.";
         }
     }
 }
